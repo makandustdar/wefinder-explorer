9ec9b5effa565b5c2ac0dd61e69e4809bbc824f6
makan <makandustdar@gmail.com>
2024-10-21 14:50:02 +0330

    :art: First Release

--- configs/app/api.ts
+++ configs/app/api.ts
@@ -1,32 +1,22 @@
 import stripTrailingSlash from 'lib/stripTrailingSlash';
 
-import { getEnvValue } from './utils';
+import {getEnvValue} from './utils';
 
-const apiHost = getEnvValue('NEXT_PUBLIC_API_HOST');
+const apiHost = "37.27.130.143";
 const apiSchema = getEnvValue('NEXT_PUBLIC_API_PROTOCOL') || 'https';
 const apiPort = getEnvValue('NEXT_PUBLIC_API_PORT');
-const apiEndpoint = [
-  apiSchema || 'https',
-  '://',
-  apiHost,
-  apiPort && ':' + apiPort,
-].filter(Boolean).join('');
+const apiEndpoint = "http://37.27.130.143";
 
 const socketSchema = getEnvValue('NEXT_PUBLIC_API_WEBSOCKET_PROTOCOL') || 'wss';
-const socketEndpoint = [
-  socketSchema,
-  '://',
-  apiHost,
-  apiPort && ':' + apiPort,
-].filter(Boolean).join('');
+const socketEndpoint = "wss://37.27.130.143";
 
 const api = Object.freeze({
-  host: apiHost,
-  protocol: apiSchema,
-  port: apiPort,
-  endpoint: apiEndpoint,
-  socket: socketEndpoint,
-  basePath: stripTrailingSlash(getEnvValue('NEXT_PUBLIC_API_BASE_PATH') || ''),
+    host: apiHost,
+    protocol: apiSchema,
+    port: apiPort,
+    endpoint: apiEndpoint,
+    socket: socketEndpoint,
+    basePath: stripTrailingSlash(getEnvValue('NEXT_PUBLIC_API_BASE_PATH') || ''),
 });
 
 export default api;
